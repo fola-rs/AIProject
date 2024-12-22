94bbac13051878a59aea0a2b6b4b9e8faa2f4d3c
fola-rs <folashodipo@gmail.com>
2024-12-22 15:57:26 +0000

    Initial commit

---
+++ shopnotesAI/src/main/java/com/shopnotesAI/shopnotesAI/ShopnotesAiApplication.java
@@ -0,0 +1,13 @@
+package com.shopnotesAI.shopnotesAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopnotesAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopnotesAiApplication.class, args);
+	}
+
+}
---
+++ shopnotesAI/src/test/java/com/shopnotesAI/shopnotesAI/ShopnotesAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopnotesAI.shopnotesAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopnotesAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
